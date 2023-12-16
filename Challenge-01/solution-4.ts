64ede4cd047b9aaac44856efb7c340452575fd88
JulitoxD <91586835+JulitoxD@users.noreply.github.com>
2023-12-16 09:30:38 +0100

    new files

--- Challenge-01/solution-4.ts
+++
@@ -1,4 +0,0 @@
-export function findFirstRepeated(gifts: number[]): number {
-  const repeated: number = gifts.filter((gift, i) => gifts.indexOf(gift) !== i )[0];
-  return (repeated) ? repeated : -1;
-}
--- Challenge-02/solution-3.ts
+++
@@ -1,7 +0,0 @@
-export function manufacture(gifts: string[], materials: string): string[] {
-  if( !gifts || !materials ) throw new Error('Invalid input');
-
-  return gifts.filter(gift => 
-    [...gift].every(character => materials.includes(character))
-  );
-}
